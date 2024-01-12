acc195e562ed45d4beab4e1804c6d2791014c22d
wjeongchoi <artificialmint@gmail.com>
2024-01-12 23:07:36 +0900

    :fire: delete previous template

--- front/src/components/Tab2.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Tab2: React.FC = () => {
-    return <div>탭 2의 내용입니다.</div>;
-};
-
-export default Tab2;
--- front/src/components/Tab3.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Tab3: React.FC = () => {
-    return <div>탭 3의 내용입니다.</div>;
-};
-
-export default Tab3;
