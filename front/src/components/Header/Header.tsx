7e52e173c4ac596e0278ab0d8dc313343318d3da
wjeongchoi <artificialmint@gmail.com>
2024-01-13 01:01:46 +0900

    :sparkles: header navigation

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -1,9 +1,5 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import { Logo } from "../Logo";
 import "./style.css";
 
@@ -13,17 +9,27 @@ interface Props {
 }
 
 export const Header = ({ className, divClassName }: Props): JSX.Element => {
+  const navigate = useNavigate();
+
+  // Existing navigation functions
+  const navigateToHome = () => navigate("/");
+  const navigateToClassList = () => navigate("/classList");
+  const navigateToClassRecomm = () => navigate("/classRecomm");
+
+  // New navigation function for myPage
+  const navigateToMyPage = () => navigate("/myPage");
+
   return (
     <div className={`header ${className}`}>
       <div className="frame">
-        <div className={`text-wrapper-2 ${divClassName}`}>홈</div>
-        <div className="text-wrapper-3">강의 목록</div>
-        <div className="text-wrapper-4">강의 추천</div>
+        <div className={`text-wrapper-2 ${divClassName}`} onClick={navigateToHome}>홈</div>
+        <div className="text-wrapper-3" onClick={navigateToClassList}>강의 목록</div>
+        <div className="text-wrapper-4" onClick={navigateToClassRecomm}>강의 추천</div>
       </div>
-      <div className="overlap">
+      <div className="overlap" onClick={navigateToMyPage}>
         <img className="person-sharp" alt="Person sharp" src="/img/person-sharp-1.svg" />
       </div>
-      <Logo className="logo-instance" />
+      <Logo className="logo-instance" onClick={navigateToHome} />
     </div>
   );
 };
