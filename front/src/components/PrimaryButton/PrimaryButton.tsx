e70887a0b06f8599f3511f7bafea3fc992a82657
wjeongchoi <artificialmint@gmail.com>
2024-01-13 17:38:37 +0900

    :sparkles: primary button

---
+++ front/src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const PrimaryButton = (): JSX.Element => {
+    return (
+        <div className="primary-button">
+            <div className="text-wrapper">선택지 1</div>
+        </div>
+    );
+};
