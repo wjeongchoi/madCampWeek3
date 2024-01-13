d2557ba0e971893d3dbe8d7d187281d0b30cca34
wjeongchoi <artificialmint@gmail.com>
2024-01-14 04:02:31 +0900

    :construction: class recomm result

---
+++ front/src/screens/ClassRecommResult/ClassRecommResult.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style.css";
+import { Header } from "../../components/Header";
+import { LongLectureBox } from "../../components/LongLectureBox/LongLectureBox";
+
+export const ClassRecommResult = (): JSX.Element => {
+    return (
+        <div className="class-recomm-result">
+            <div className="div-2">
+                <Header
+                    className="header-instance"
+                    activePage="classRecomm"
+                    divClassName="design-component-instance-node"
+                />
+                <div className="frame-2">
+                    <div className="group">
+                        <p className="a-ipple">
+                            <span className="span">당신을 위한 </span>
+                            <span className="text-wrapper-6">AIpple</span>
+                            <span className="span">의 추천 강의</span>
+                        </p>
+                        <p className="p">
+                            <span className="span">딥러닝</span>
+                            <span className="text-wrapper-7">에 관심이 있는</span>
+                        </p>
+                    </div>
+                    <div className="frame-3">
+                        <LongLectureBox />
+                        <LongLectureBox />
+                        <LongLectureBox />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ front/src/screens/ClassRecommResult/index.ts
@@ -0,0 +1,1 @@
+export { ClassRecommResult} from "./ClassRecommResult";
