aaf1095adedb930e716a5a125d42e1a95edc90a6
wjeongchoi <artificialmint@gmail.com>
2024-01-14 04:03:52 +0900

    :sparkles: add button navigate

--- front/src/screens/ClassRecomm/ClassRecomm.tsx
+++ front/src/screens/ClassRecomm/ClassRecomm.tsx
@@ -3,8 +3,14 @@ import React from "react";
 import "./style.css";
 import { Header } from "../../components/Header";
 import { SecondaryButton } from "../../components/SecondaryButton";
+import { useNavigate } from "react-router";
 
 export const ClassRecomm = (): JSX.Element => {
+    const navigate = useNavigate();
+
+    const navigateToClassRecommAsk = () => {
+      navigate("/classRecommAsk");
+    };
     return (
         <div className="class-recomm">
             <div className="div-2">
@@ -18,7 +24,8 @@ export const ClassRecomm = (): JSX.Element => {
                         <span className="text-wrapper-6">AIpple</span>
                         <span className="span">의 강의 추천</span>
                     </p>
-                    <SecondaryButton label="바로 시작하기"/>
+                    <SecondaryButton label="바로 시작하기" onClick={navigateToClassRecommAsk}
+/>
                 </div>
             </div>
         </div>
