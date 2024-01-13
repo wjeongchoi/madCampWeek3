8e3a6aec70eb318caddbe76c338f7277a02cf4f7
wjeongchoi <artificialmint@gmail.com>
2024-01-13 22:23:12 +0900

    :lipstick: edit classRecommAsk

--- front/src/screens/ClassRecommAsk/ClassRecommAsk.tsx
+++ front/src/screens/ClassRecommAsk/ClassRecommAsk.tsx
@@ -8,18 +8,17 @@ export const ClassRecommAsk = (): JSX.Element => {
   return (
     <div className="class-recomm-ask">
       <Header
-          className="header-instance"
-          activePage="classRecomm"
-          divClassName="design-component-instance-node"
-        />
+        className="header-instance"
+        activePage="classRecomm"
+        divClassName="design-component-instance-node"
+      />
       <div className="div-2">
-        
-          <div className="text-wrapper-4">어떤 분야에 관심이 있나요?</div>
-          <div className="frame-2">
-            <PrimaryButton label="선택지 1" />
-            <PrimaryButton label="선택지 2" />
-            <PrimaryButton label="선택지 3" />
-          </div>
+        <div className="text-wrapper-4">어떤 분야에 관심이 있나요?</div>
+        <div className="frame-2">
+          <PrimaryButton label="선택지 1" />
+          <PrimaryButton label="선택지 2" />
+          <PrimaryButton label="선택지 3" />
+        </div>
       </div>
     </div>
   );
