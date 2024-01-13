185c5a3c7897a5161ac131997744dd7eeb56d0f6
wjeongchoi <artificialmint@gmail.com>
2024-01-13 20:25:47 +0900

    :lipstick: class recomm ask

--- front/src/screens/ClassRecommAsk/ClassRecommAsk.tsx
+++ front/src/screens/ClassRecommAsk/ClassRecommAsk.tsx
@@ -7,18 +7,19 @@ import { PrimaryButton } from "../../components/PrimaryButton";
 export const ClassRecommAsk = (): JSX.Element => {
   return (
     <div className="class-recomm-ask">
-      <div className="div-2">
-        <Header
+      <Header
           className="header-instance"
           activePage="classRecomm"
           divClassName="design-component-instance-node"
         />
-        <div className="text-wrapper-4">어떤 분야에 관심이 있나요?</div>
-        <div className="frame-2">
-          <PrimaryButton />
-          <PrimaryButton />
-          <PrimaryButton />
-        </div>
+      <div className="div-2">
+        
+          <div className="text-wrapper-4">어떤 분야에 관심이 있나요?</div>
+          <div className="frame-2">
+            <PrimaryButton label="선택지 1" />
+            <PrimaryButton label="선택지 2" />
+            <PrimaryButton label="선택지 3" />
+          </div>
       </div>
     </div>
   );
