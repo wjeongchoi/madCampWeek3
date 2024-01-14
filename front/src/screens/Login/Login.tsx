0476b9c79a2018c6ea310a33ec4171396a557740
wjeongchoi <artificialmint@gmail.com>
2024-01-15 03:00:14 +0900

    :lipstick: login & signup layout

--- front/src/screens/Login/Login.tsx
+++ front/src/screens/Login/Login.tsx
@@ -6,21 +6,21 @@ import { InputText } from "../../components/InputText";
 import { SecondaryButton } from "../../components/SecondaryButton";
 
 export const Login = (): JSX.Element => {
-    return (
-        <div className="login">
-            <div className="div-2">
-                <Header className="header-instance" />
-                <div className="frame-2">
-                    <div className="group">
-                        <div className="frame-3">
-                            <InputText  label="아이디"/>
-                            <InputText label="비밀번호" />
-                        </div>
-                        <SecondaryButton label="로그인" />
-                    </div>
-                    <div className="text-wrapper-5">로그인</div>
-                </div>
+  return (
+    <div className="login">
+      <div className="div-2">
+        <Header className="header-instance" />
+        <div className="frame-2">
+          <div className="frame-3">
+            <div className="frame-4">
+              <div className="text-wrapper-5">로그인</div>
+              <InputText label="아이디" />
+              <InputText label="비밀번호" />
             </div>
+            <SecondaryButton label="로그인" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
