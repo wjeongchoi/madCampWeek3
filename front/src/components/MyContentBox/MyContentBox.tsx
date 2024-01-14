cb03f63d894abe29d87a26452d4216c0def2c24d
wjeongchoi <artificialmint@gmail.com>
2024-01-14 22:06:36 +0900

    :lipstick: edit mycontentbox

--- front/src/components/MyContentBox/MyContentBox.tsx
+++ front/src/components/MyContentBox/MyContentBox.tsx
@@ -6,8 +6,8 @@ export const MyContentBox = (): JSX.Element => {
         <div className="my-content-box">
             <div className="frame">
                 <div className="div">
-                    <div className="text-wrapper">CS000</div>
-                    <div className="text-wrapper-2">Lecture 0</div>
+                    <div className="text-wrapper">Lecture 0</div>
+                    <div className="text-wrapper-2">2024-01-14</div>
                 </div>
                 <div className="text-wrapper-3">수업 필기</div>
             </div>
