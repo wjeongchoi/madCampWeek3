ad0d98cde81f47ab23d34fd4b8de2a9475afc697
wjeongchoi <artificialmint@gmail.com>
2024-01-14 22:04:47 +0900

    :sparkles: input text component

---
+++ front/src/components/InputText/InputText.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const InputText = (): JSX.Element => {
+    return (
+        <div className="input-text">
+            <div className="text-wrapper">아이디</div>
+        </div>
+    );
+};
