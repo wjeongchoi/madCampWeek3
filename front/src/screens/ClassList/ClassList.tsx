6223efd45fefea602d91e859cb53836ac46024a6
wjeongchoi <artificialmint@gmail.com>
2024-01-14 14:34:04 +0900

    :construction: classList

--- front/src/screens/ClassList/ClassList.tsx
+++ front/src/screens/ClassList/ClassList.tsx
@@ -1,37 +1,24 @@
 import React from "react";
+
+import "./style.css";
 import { Header } from "../../components/Header";
 import { LectureBox } from "../../components/LectureBox";
-import { SideBar } from "../../components/SideBar";
-import "./style.css";
 
 export const ClassList = (): JSX.Element => {
   return (
     <div className="class-list">
       <div className="overlap-wrapper">
+        <Header
+          className="header-instance"
+          divClassName="design-component-instance-node"
+          activePage="classList"
+        />
         <div className="overlap-2">
-          <div className="overlap-3">
-            <SideBar
-              className="side-bar-instance"
-              sidebarButtonDivClassName="design-component-instance-node"
-              sidebarButtonPropertyClassName="design-component-instance-node"
-              sidebarButtonPropertyClassNameOverride="design-component-instance-node"
-              sidebarButtonPropertyDefaultClassName="design-component-instance-node"
-              sidebarButtonPropertyDefaultClassNameOverride="design-component-instance-node"
-              sidebarButtonPropertyVariantClassName="design-component-instance-node"
-              sidebarButtonPropertyVariantClassNameOverride="design-component-instance-node"
-            />
-            <Header
-              className="header-instance"
-              divClassName="design-component-instance-node"
-              activePage="classList"
-            />
-          </div>
-          <div className="frame-3">
+          <div className="frame-2">
             <LectureBox />
             <LectureBox />
             <LectureBox />
             <LectureBox />
-        
           </div>
         </div>
       </div>
