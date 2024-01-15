728bea6844ee22fb947619b9e34457fca498be84
wjeongchoi <artificialmint@gmail.com>
2024-01-15 14:23:05 +0900

    :sparkles: allow input for lecture box

--- front/src/components/LectureBox/LectureBox.tsx
+++ front/src/components/LectureBox/LectureBox.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./style.css";
 
-export const LectureBox = (): JSX.Element => {
+// Define an interface for the component props
+interface LectureBoxProps {
+    title: string;         // Prop for the lecture title
+    description: string;   // Prop for the lecture description
+    imageUrl: string;      // Prop for the lecture image URL
+    date: string;          // Prop for the lecture date
+}
+
+// Update the component to accept props
+export const LectureBox = ({ title, description, imageUrl, date }: LectureBoxProps): JSX.Element => {
     return (
         <div className="lecture-box">
             <div className="frame">
-                <p className="text-wrapper">[CS000] Convolutional Neural Networks for Visual Recognition</p>
-                <img className="image" alt="Image" src="/img/image-1.png" />
-                <div className="div">2024-01-14</div>
-                <div className="text-wrapper-2">
-                    설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명
-                </div>
+                <p className="text-wrapper">{title}</p>  {/* Use title prop */}
+                <img className="image" alt="Lecture image" src={imageUrl} /> {/* Use imageUrl prop */}
+                <div className="div">{date}</div> {/* Use date prop */}
+                <div className="text-wrapper-2">{description}</div> {/* Use description prop */}
             </div>
         </div>
     );
--- front/src/screens/ClassRecommResult/ClassRecommResult.tsx
+++ front/src/screens/ClassRecommResult/ClassRecommResult.tsx
@@ -4,33 +4,44 @@ import { Header } from "../../components/Header";
 import { LectureBox } from "../../components/LectureBox";
 
 export const ClassRecommResult = (): JSX.Element => {
-    return (
-        <div className="class-recomm-result">
-            <div className="div-2">
-                <Header
-                    className="header-instance"
-                    activePage="classRecomm"
-                />
-                <div className="frame-2">
-                    <div className="group">
-                        <p className="a-ipple">
-                            <span className="span">당신을 위한 </span>
-                            <span className="text-wrapper-6">AIpple</span>
-                            <span className="span">의 추천 강의</span>
-                        </p>
-                        <p className="p">
-                            <span className="span">딥러닝</span>
-                            <span className="text-wrapper-7">에 관심이 있는</span>
-                        </p>
-                    </div>
-                    <div className="frame-3">
-                        <LectureBox />
-                        <LectureBox />
-                        <LectureBox />
-
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="class-recomm-result">
+      <div className="div-2">
+        <Header className="header-instance" activePage="classRecomm" />
+        <div className="frame-2">
+          <div className="group">
+            <p className="a-ipple">
+              <span className="span">당신을 위한 </span>
+              <span className="text-wrapper-6">AIpple</span>
+              <span className="span">의 추천 강의</span>
+            </p>
+            <p className="p">
+              <span className="span">딥러닝</span>
+              <span className="text-wrapper-7">에 관심이 있는</span>
+            </p>
+          </div>
+          <div className="frame-3">
+            <LectureBox
+              title="[CS101] Introduction to Programming"
+              description="Learn the basics of programming with Python..."
+              imageUrl="/img/image-1.png"
+              date="2024-02-01"
+            />
+            <LectureBox
+              title="[CS101] Introduction to Programming"
+              description="Learn the basics of programming with Python..."
+              imageUrl="/img/image-1.png"
+              date="2024-02-01"
+            />
+            <LectureBox
+              title="[CS101] Introduction to Programming"
+              description="Learn the basics of programming with Python..."
+              imageUrl="/img/image-1.png"
+              date="2024-02-01"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
