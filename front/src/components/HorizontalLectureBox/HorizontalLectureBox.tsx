cf5aba2a8f2a1068b6b78856f13d58fb5c532846
wjeongchoi <artificialmint@gmail.com>
2024-01-16 15:13:02 +0900

    :sparkles: clickable horizontal lecture box

--- front/src/components/HorizontalLectureBox/HorizontalLectureBox.tsx
+++ front/src/components/HorizontalLectureBox/HorizontalLectureBox.tsx
@@ -3,14 +3,14 @@ import "./style.css";
 
 interface HorizontalLectureBoxProps {
     selected: boolean;
-    className?: string;
-    title: string; // Prop for the lecture title
-    date: string; // Prop for the lecture date
-}
-
-export const HorizontalLectureBox = ({ selected, className = "", title, date }: HorizontalLectureBoxProps): JSX.Element => {
+    title: string;
+    date: string;
+    onClick: () => void;
+  }
+  
+  export const HorizontalLectureBox: React.FC<HorizontalLectureBoxProps> = ({ selected, title, date, onClick }) => {
     return (
-        <div className={`horizontal-lecture selected-${selected} ${className}`}>
+      <div className={`horizontal-lecture-box ${selected ? 'selected' : ''}`} onClick={onClick}>
             <div className="frame">
                 <div className="text-wrapper">{title}</div>
                 <div className="div">{date}</div>
--- front/src/screens/MyPage/MyPage.tsx
+++ front/src/screens/MyPage/MyPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./style.css";
@@ -7,14 +7,36 @@ import { HorizontalLectureBox } from "../../components/HorizontalLectureBox";
 import { MyContentBox } from "../../components/MyContentBox";
 
 export const MyPage: React.FC = (): JSX.Element => {
+  const [selectedLecture, setSelectedLecture] = useState<number | null>(null);
+
   const navigate = useNavigate();
-
-
 
   const handleLogout = () => {
     localStorage.removeItem("userID");
     navigate("/");
   };
+
+  const selectLecture = (lectureId: number) => {
+    setSelectedLecture(lectureId);
+  };
+
+  const lectures = [
+    {
+      id: 0,
+      title:
+        "Lecture Collection | Convolutional Neural Networks for Visual Recognition (Spring 2017)",
+      desc: "Computer Vision has become ubiquitous in our society, with applications in search, image understanding, apps, mapping, medicine, drones, and self-driving cars. Core to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This lecture collection is a deep dive into details of the deep learning architectures with a focus on learning end-to-end models for these tasks, particularly image classification. From this lecture collection, students will learn to implement, train and debug their own neural networks and gain a detailed understanding of cutting-edge research in computer vision.",
+      thumbnail_url: null,
+      date: "2024-01-01",
+    },
+    {
+      id: 1,
+      title: "asdf",
+      desc: "asdf",
+      thumbnail_url: null,
+      date: "2024-01-01",
+    },
+  ];
 
   return (
     <div className="my-page">
@@ -28,32 +50,14 @@ export const MyPage: React.FC = (): JSX.Element => {
         <div className="frame-5">
           <div className="frame-6">
             <div className="frame-7">
-              <HorizontalLectureBox
-                selected={true}
-                title="[CS000] Introduction to Computer Science"
-                date="2024-01-14"
-              />
-              <HorizontalLectureBox
-                selected={false}
-                title="[CS000] Introduction to Computer Science"
-                date="2024-01-14"
-              />
-
-              <HorizontalLectureBox
-                selected={false}
-                title="[CS000] Introduction to Computer Science"
-                date="2024-01-14"
-              />
-              <HorizontalLectureBox
-                selected={false}
-                title="[CS000] Introduction to Computer Science"
-                date="2024-01-14"
-              />
-              <HorizontalLectureBox
-                selected={false}
-                title="[CS000] Introduction to Computer Science"
-                date="2024-01-14"
-              />
+              {lectures.map((lecture) => (
+                <HorizontalLectureBox
+                  selected={selectedLecture === lecture.id} // Assuming each lecture has a unique 'id'
+                  title={lecture.title}
+                  date={lecture.date}
+                  onClick={() => selectLecture(lecture.id)}
+                />
+              ))}
             </div>
             <div className="text-wrapper-8">학습 중인 강의</div>
           </div>
