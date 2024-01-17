fa340c7edd7eab8d67e6e6f4debd8c68c2665000
Seongah Kim <kimsa0322@naver.com>
2024-01-17 16:08:46 +0900

    auto scroll

--- front/src/screens/Home/Home.tsx
+++ front/src/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Header } from "../../components/Header";
 import { Logo } from "../../components/Logo";
 import { SecondaryButton } from "../../components/SecondaryButton";
@@ -8,6 +8,30 @@ import { useNavigate } from "react-router";
 
 export const Home = (): JSX.Element => {
   const navigate = useNavigate();
+  const frame5Ref = useRef<HTMLDivElement>(null);
+  const [isScrollingRight, setIsScrollingRight] = useState(true); // 스크롤 방향 상태
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+    if (frame5Ref.current) {
+      const maxScrollLeft = frame5Ref.current.scrollWidth - frame5Ref.current.clientWidth; // 40ms 마다 스크롤
+      if (isScrollingRight) {
+        if (frame5Ref.current.scrollLeft < maxScrollLeft) {
+          frame5Ref.current.scrollLeft += 1;
+        } else {
+          setIsScrollingRight(false); // 오른쪽 끝에 도달했으므로 방향 전환
+        }
+      } else {
+        if (frame5Ref.current.scrollLeft > 0) {
+          frame5Ref.current.scrollLeft -= 1;
+        } else {
+          setIsScrollingRight(true); // 왼쪽 끝에 도달했으므로 방향 전환
+        }
+      }
+    }
+  }, 40); // 40ms 마다 스크롤 위치 업데이트
+    return () => clearInterval(timer); // 컴포넌트가 언마운트될 때 타이머 정리
+  }, []);
 
   const navigateToClassRecomm = () => {
     navigate("/classRecomm");
@@ -34,13 +58,13 @@ export const Home = (): JSX.Element => {
               <span className="text-wrapper-8"> 인기 강의</span>
             </p>
           </div>
-          <div className="frame-5">
+          <div className="frame-5" ref={frame5Ref}>
             <SmallLectureBox lectureId={"0"} />
             <SmallLectureBox lectureId={"1"} />
             <SmallLectureBox lectureId={"3"} />
             <SmallLectureBox lectureId={"5"} />
             <SmallLectureBox lectureId={"4"} />
-          </div>
+      </div>
         </div>
         <div className="frame-3">
           <div className="a-ipple-wrapper">
