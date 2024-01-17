a07d5382972a55db088c934d00543c6349abf923
wjeongchoi <artificialmint@gmail.com>
2024-01-17 12:54:49 +0900

    :lipstick: class recomm result layout

--- front/src/screens/ClassRecommResult/ClassRecommResult.tsx
+++ front/src/screens/ClassRecommResult/ClassRecommResult.tsx
@@ -74,14 +74,14 @@ export const ClassRecommResult = (): JSX.Element => {
         <Header className="header-instance" activePage="classRecomm" />
         <div className="frame-2">
           <div className="group">
+            <p className="p">
+              <span className="span">{interestArea}</span>
+              <span className="text-wrapper-7">에 관심이 있는</span>
+            </p>
             <p className="a-ipple">
               <span className="span">당신을 위한 </span>
               <span className="text-wrapper-6">AIpple</span>
               <span className="span">의 추천 강의</span>
-            </p>
-            <p className="p">
-              <span className="span">{interestArea}</span>
-              <span className="text-wrapper-7">에 관심이 있는</span>
             </p>
           </div>
           <div className="frame-3">
